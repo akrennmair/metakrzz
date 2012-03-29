2df8da9ea2c1888dd8704cc69018ed8effa40c6e
Andreas Krennmair <ak@synflood.at>
2012-03-30 00:29:39 +0200

    query server (not functional yet).

--- cgi-bin/metakrzz.pl
+++ cgi-bin/metakrzz.pl
@@ -21,10 +21,29 @@ my $html = <<END;
 <body>
 <div class="container">
 <h2>meta.krzz.de Meta URL Shortener</h2>
-<form action="" method="post" class="well">
-<input id=url name=url type=url placeholder="URL, e.g. http://example.com/" required autofocus class="input-medium" style="width: 100%">
-<button id=btn class="btn btn-primary" type=submit>Shorten</button>
-</form>
+<div class="row">
+	<div class="span12">
+		<form action="" method="post" class="well">
+		<input id=url name=url type=url placeholder="URL, e.g. http://example.com/" required autofocus class="input-medium" style="width: 100%">
+		<button id=btn class="btn btn-primary" type=submit>Shorten</button>
+		</form>
+	</div>
+</div>
+<div class="row">
+	<div id="errmsgbox" class="alert alert-error span4" style="display: none"></div>
+</div>
+<div class="row" id="recvmsg" style="display: none">
+	<div class="span12">
+		Receiving results...
+		<img src="/files/img/indicator.gif">
+	</div>
+</div>
+<div class="row">
+	<table class="table table-bordered table-striped table-condensed span12" id="urltbl" style="display: none">
+		<tr><th>URL</th><th>Length</th></tr>
+	</table>
+</div>
+</div>
 </body>
 </html>
 END
@@ -32,3 +51,11 @@ END
 get '/' => sub {
 	return $html;
 };
+
+get '/shorten/(.*)' => sub {
+	my $id = shift;
+	content_type("application/json");
+	sleep(rand(10));
+	return "{ \"url\": \"http://foobar.com/$id\" }";
+};
+
--- files/js/app.dart
+++ files/js/app.dart
@@ -1,11 +1,81 @@
 #import('dart:html');
+#import('dart:json');
 
 class UI {
-	int count = 0;
 
-	buttonClicked() {
-		print('click ' + this.count + '!');
-		this.count++;
+	var shorteners;
+	
+	UI() {
+		this.shorteners = [ 'googl', 'tinyurl' ];
+	}
+
+	void showError(String msg) {
+		DivElement errmsg = document.query('#errmsgbox');
+		errmsg.innerHTML = '<a id="errmsgclose" class="close" data-dismiss="alert">&times;</a><h4>Error:</h4>$msg';
+		errmsg.style.display = "inline";
+		document.query('#errmsgclose').on.click.add( (e) => errmsg.style.display = "none" );
+	}
+
+	void buttonClicked() {
+		InputElement url_input = document.query('#url');
+		if (!url_input.checkValidity()) {
+			showError("Invalid URL");
+			return;
+		}
+
+		int results_open = shorteners.length;
+		document.query('#recvmsg').style.display = "inline";
+		print("activated recvmsg");
+
+		TableElement tbl = document.query('#urltbl');
+		tbl.style.display = "inline";
+		print("showing urltbl");
+		// TODO: empty table beforehand
+
+		String url = url_input.value;
+
+		shorteners.forEach((id) {
+			print("id = $id");
+			XMLHttpRequest xhr = new XMLHttpRequest();
+			xhr.open("GET", "/shorten/" + id + "?url=" + url, true);
+
+			xhr.on.load.add((event) {
+				print("got load for $id");
+				if (xhr.status != 200) {
+					print("error: " + xhr.responseText);
+					showError("shortener returned " + xhr.status);
+					return;
+				}
+				Map<String, Object> msg = JSON.parse(xhr.responseText);
+				if (msg["error"] != null) {
+					showError(msg["error"]);
+					print(msg["error"]);
+				} else {
+					String url = msg["url"];
+
+					TableRowElement new_row = tbl.insertRow(tbl.rows.length);
+
+					TableCellElement urlfield = new Element.html('<td><a href="${url}">${url}</a></td>');
+					new_row.nodes.add(urlfield);
+
+					TableCellElement lenfield = new Element.tag('td');
+					lenfield.text = '${url.length} characters';
+					new_row.nodes.add(lenfield);
+
+					results_open--;
+
+					print("results_open = $results_open");
+
+					if (results_open == 0) {
+						document.query('#recvmsg').style.display = "none";
+						print("deactviated recvmsg");
+					}
+				}
+			});
+
+			xhr.send();
+			print("sent request for $id");
+		});
 	}
 }
 
